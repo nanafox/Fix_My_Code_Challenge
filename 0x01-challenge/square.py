662e1a1de42aebd1a6914c312ef45006a4bfe50d
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-04-07 23:35:47 +0000

    Add Square class with area and perimeter methods

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""This module defines a Square class."""
+
+
+class Square:
+    """Defines the Square class."""
+
+    def __init__(self, size):
+        """Initializes the Square instance."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Get the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square."""
+        if not isinstance(value, int):
+            raise ValueError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area_of_my_square(self):
+        """Area of the square"""
+        return self.__size ** 2
+
+    def permiter_of_my_square(self):
+        """Returns the perimeter of the square."""
+        return self.__size * 4
+
+    def __str__(self):
+        return f"{self.size}/{self.size}"
+
+
+if __name__ == "__main__":
+
+    s = Square(size=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.permiter_of_my_square())
